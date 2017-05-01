1557d7ef16acc41e045d0b2261577dea6ade3fe4
Dongwoon Son <dplusic@gmail.com>
2017-05-01 23:05:39 +0900

    Setup redux-form

--- app.src/app/reducers.js
+++ app.src/app/reducers.js
@@ -8,6 +8,7 @@ import { fromJS } from 'immutable';
 import { LOCATION_CHANGE } from 'react-router-redux';
 
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
+import { reducer as reduxFormReducer } from 'redux-form/immutable';
 
 /*
  * routeReducer
@@ -44,6 +45,7 @@ export default function createReducer(asyncReducers) {
   return combineReducers({
     route: routeReducer,
     language: languageProviderReducer,
+    form: reduxFormReducer,
     ...asyncReducers,
   });
 }
