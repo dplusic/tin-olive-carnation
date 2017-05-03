de804d7e4c446f29b0649e6c9e07d1f081cb7a27
Dongwoon Son <dplusic@gmail.com>
2017-05-04 00:24:57 +0900

    Call cognito user pool sign up api

--- app.src/app/containers/SignUpPage/sagas.js
+++ app.src/app/containers/SignUpPage/sagas.js
@@ -1,8 +1,27 @@
-import { take, takeLatest, cancel } from 'redux-saga/effects';
+import { take, takeLatest, cancel, cps } from 'redux-saga/effects';
 import { LOCATION_CHANGE } from 'react-router-redux';
+import { CognitoUserPool, CognitoUserAttribute } from 'amazon-cognito-identity-js';
 import { SIGN_UP } from './constants';
 
-export function signUp() {
+export function* signUp({ values }) {
+  const username = values.get('username');
+  const email = values.get('email');
+  const password = values.get('password');
+
+  const userPool = new CognitoUserPool({
+    UserPoolId: 'ap-northeast-2_W6d2OguRO',
+    ClientId: '6idn80s1spobefoddgrq6dfrt',
+  });
+  const attributeList = [
+    new CognitoUserAttribute({ Name: 'email', Value: email }),
+  ];
+
+  try {
+    const result = yield cps([userPool, userPool.signUp], username, password, attributeList, null);
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 // Individual exports for testing
