5ea309c8c26be49598c891ca08e58492f855195f
Dongwoon Son <dplusic@gmail.com>
2017-05-21 21:27:52 +0900

    Call authorized api gateway method

--- app.src/app/containers/HomePage/index.js
+++ app.src/app/containers/HomePage/index.js
@@ -12,9 +12,29 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router';
+import AWS from 'aws-sdk';
+import apigClientFactory from 'aws-api-gateway-client';
 import messages from './messages';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+
+  // TODO: temp
+  async componentWillMount() {
+    try {
+      const apigClient = apigClientFactory.newClient({
+        accessKey: AWS.config.credentials.accessKeyId,
+        secretKey: AWS.config.credentials.secretAccessKey,
+        sessionToken: AWS.config.credentials.sessionToken,
+        region: AWS.config.region,
+        invokeUrl: 'https://mvye08207h.execute-api.ap-northeast-2.amazonaws.com/dev',
+      });
+      const result = await apigClient.invokeApi({}, '/hello', 'get');
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <div>
--- handlers/handler.js
+++ handlers/handler.js
@@ -3,6 +3,10 @@
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+    },
     body: JSON.stringify({
       message: 'Welcome to tin-olive-carnation!',
       input: event,
